738f94fd7ad6285ba4a581fd1604fa02da912fad
Stefan Redfield <redfield.stefan@gmail.com>
2015-04-28 09:53:28 -0700

    Create test for greeter function

--- test/greetTest.js
+++ test/greetTest.js
@@ -1,2 +1,18 @@
 'use strict';
 
+var expect = require('chai').expect;
+var Greeter = require('../greeter');
+
+describe("greeter.js test", function() {
+
+	var informalGreeter;
+
+	before(function(){
+		informalGreeter = new Greeter();
+	});
+
+	it('Should return an informal greeting for the inputed name', function() {
+		expect(informalGreeter.greet('Stefan')).to.eql("Stefan, my man, whaddup?");
+	});
+
+});
